0699162bcc1e9cf9c489658ac2e505786baa5e40
Arturo Rivera <arturo.riverar97@gmail.com>
2022-04-06 18:49:28 -0500

    Create functions to paint RGB and black
    RGBorBLACK function decides if the pixel shoudl be given a class among
    green, red or blue; or if its color should have a black hue.
    
    In order to get a random class among red, green or blue, Math random
    was used to get integers from 0 to 2, which would be assigned a class.

--- javascript.js
+++ javascript.js
@@ -24,11 +24,55 @@ function createContainers(mainContainer) {
         mainContainer.appendChild(div);
     }
     const pixel = document.querySelectorAll('.pixel');
-    pixel.forEach(container => container.addEventListener('mouseover', paintBlack));
+    pixel.forEach(container => container.addEventListener('mouseover', RGBorBlack));
 }
 
 function paintBlack(event) {
     event.target.classList.add('black');
+    //console.log(window.getComputedStyle(event.target).backgroundColor); //Se obtiene RGB
+    //console.log(event.target.classList.length) // Se obtiene numero de elementos en la clase.
+    //console.log(event.target.className.split(' ')[1]) // Se obtiene el nombre de segunda clase.
+}
+
+function getRandomRGBClass(){
+    let maxNum = 3;
+    let numberColor = Math.floor(Math.random()*maxNum); // [0,2]
+
+    if(numberColor == 0){
+        return 'red';
+    } else if(numberColor == 1){
+        return 'green';
+    } else {
+        return 'blue';
+    }
+}
+
+function addBlack(event){
+    let RGB = window.getComputedStyle(event.target).backgroundColor;
+    rgbArr = RGB.substring(4, RGB.length-1).replace(/ /g, '').split(',');
+    
+    let red = rgbArr[0];
+    let green = rgbArr[1];
+    let blue = rgbArr[2];
+
+    let stepToBlack = 255*0.1;
+
+    let newColor = `rgb(${red-stepToBlack},${green-stepToBlack},${blue-stepToBlack})`;
+
+    event.target.style.backgroundColor = newColor;
+}
+
+function RGBorBlack(event) {
+    if(event.target.classList.length == 2){
+        addBlack(event);
+    } else {
+        getRandomRGB(event);
+    }
+}
+
+function getRandomRGB(event) {
+    let nameClass = getRandomRGBClass();
+    event.target.classList.add(nameClass);
 }
 
 function cleanSlate(){
